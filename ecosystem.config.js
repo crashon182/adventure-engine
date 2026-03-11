d7acbc2cf4381dac7121929d309c2b669e87ffff
Alexis <alexis@adventure-engine.local>
2026-03-11 08:45:32 -0300

    Fix 502 error

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,8 @@ module.exports = {
                 NODE_ENV: 'production',
                 PORT: '3002',
             },
+            listen_timeout: 10000,
+            kill_timeout: 5000,
             // Si /usr/bin/node20 no existe, PM2 fallará al primer intento.
             // El deploy.yml se encargará de encontrar el binario correcto y reescribir este archivo.
         },
